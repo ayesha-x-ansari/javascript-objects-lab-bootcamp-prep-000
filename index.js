b874339571c223e425771c12c2e254ff2a1c0df9
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-11-22 22:26:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var obj = { prop: 1 }
 prop_2  = 'prop2'
 value_2 = 2
 function updateObjectWithKeyAndValue(obj, prop_2, value_2) {
-obj[prop_2] = value_2
-return Obj
+   Object.assign({}, obj, { [key]: value })
+   return Object
+}
+var obj = { prop: 1 }
+var key = 'prop2'
+var value = 2
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
 }
