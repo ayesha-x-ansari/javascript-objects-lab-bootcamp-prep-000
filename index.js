fcfc167184e506fa8aa9e67ce698e30c4bfc90e6
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-11-22 22:39:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ var obj = { prop: 1 }
 key  = 'prop2'
 value = 2
 function updateObjectWithKeyAndValue(obj, key, value) {
-   Object.assign({}, obj, { [key]: value })
-   return Object
+   return Object.assign({}, obj, { [key]: value })
+
 }
+
+
 var obj = { prop: 1 }
 var key = 'prop2'
 var value = 2
-
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value
   return obj
+}
+
+var obj = { prop: 1 }
+var value = prop
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
 }
