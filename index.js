9ae3e08cff742be3cd0d0c8f75dbb7b6ee2c5309
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-11-22 22:19:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var recipes = {}
 
-var obj = { prop: '1' }
-function updateObjectWithKeyAndValue(obj, 'prop2', 2) {
-obj['prop2'] = '2'
+var obj = { prop: 1 }
+prop_2  = 'prop2'
+value_2 = 2
+function updateObjectWithKeyAndValue(obj, prop_2, value_2) {
+obj[prop_2] = value_2
 return Obj
 }
